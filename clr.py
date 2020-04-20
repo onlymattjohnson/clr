9bb43130d45963d4551e517a81faceabd0325014
Matt Johnson <mjohn23@gmail.com>
2020-04-20 16:55:12 -0500

    Add base playerclass

--- clr.py
+++ clr.py
@@ -1,17 +1,24 @@
 import random
 
-def roll_dice():
-  """
-  Rolls the dice in a CLR game
-  """
-  dice_values = {1: '•', 2: '•', 3: '•', 4: 'L', 5: 'C', 6: 'R'}
-  result = random.randint(1, 6)
+class Player():
 
-  return dice_values[result]
+  def __init__(self, name):
+    self.name = name
+    self.tokens = 3
+
+  def roll_dice(self):
+    """
+    Rolls the dice in a CLR game
+    """
+    dice_values = {1: '•', 2: '•', 3: '•', 4: 'L', 5: 'C', 6: 'R'}
+    result = random.randint(1, 6)
+
+    return dice_values[result]
 
 if __name__ == '__main__':
-    dice1 = roll_dice()
-    dice2 = roll_dice()
-    dice3 = roll_dice()
+    new_player = Player(name = 'Matt')
+    dice1 = new_player.roll_dice()
+    dice2 = new_player.roll_dice()
+    dice3 = new_player.roll_dice()
 
     print(dice1, dice2, dice3)
