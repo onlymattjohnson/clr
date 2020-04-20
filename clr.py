4400fb629d280948929c6a7f90f7b131fb67709a
Matt Johnson <mjohn23@gmail.com>
2020-04-20 16:47:44 -0500

    Add dice roll function

--- clr.py
+++ clr.py
@@ -1,3 +1,17 @@
+import random
+
+def roll_dice():
+  """
+  Rolls the dice in a CLR game
+  """
+  dice_values = {1: '•', 2: '•', 3: '•', 4: 'L', 5: 'C', 6: 'R'}
+  result = random.randint(1, 6)
+
+  return dice_values[result]
 
 if __name__ == '__main__':
-  print('Hello world.')
+    dice1 = roll_dice()
+    dice2 = roll_dice()
+    dice3 = roll_dice()
+
+    print(dice1, dice2, dice3)
